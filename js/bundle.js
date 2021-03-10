32a6c836019d4bee3436215e33bf3a8881032264
zubkovvladimir <54639512+zubkovvladimir@users.noreply.github.com>
2021-03-10 13:45:32 +0000

    Deploying to gh-pages from  @ 9eb03a57f1293e093933955bd8a87a034c1996d9 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -115,13 +115,31 @@ const NAME_PATTERN = /[^-а-яА-Я ]/g;
 const PHONE_PATTERN = /[^-0-9 +]/g;
 const EMAIL_PATTERN = /[^a-zA-z]+@[^a-zA-z]+.[^a-zA-z]+/g;
 
-const NAME_WARN = 'Пожалуйста, используйте только русские буквы, пробелы и тире.';
-const PHONE_WARN = 'Пожалуйста, используйте только цифры, знаки "-" и "+" и пробелы.';
-const EMAIL_WARN = 'Пожалуйста, введите корреткный email.';
+const NAME_TEXT = 'Пожалуйста, используйте только русские буквы, пробелы и тире.';
+const PHONE_TEXT = 'Пожалуйста, используйте только цифры, знаки "-" и "+" и пробелы.';
+const EMAIL_TEXT = 'Пожалуйста, введите корреткный email.';
 
 const inputName = document.querySelector('.form__input--name');
 const inputPhone = document.querySelector('.form__input--phone');
 const inputEmail = document.querySelector('.form__input--email');
+
+const warnMap = {
+  name: {
+    input: inputName,
+    text: NAME_TEXT,
+    pattern: NAME_PATTERN
+  },
+  phone: {
+    input: inputPhone,
+    text: PHONE_TEXT,
+    pattern: PHONE_PATTERN
+  },
+  email: {
+    input: inputPhone,
+    text: EMAIL_TEXT,
+    pattern: EMAIL_PATTERN
+  }
+};
 
 // валидирует инпуты
 
@@ -136,15 +154,15 @@ const checkInputValidity = function (inputElement, pattern, warn) {
 };
 
 const onNameInput = function () {
-  checkInputValidity(inputName, NAME_PATTERN, NAME_WARN);
+  checkInputValidity(warnMap.name.input, warnMap.name.pattern, warnMap.name.text);
 };
 
 const onPhoneInput = function () {
-  checkInputValidity(inputPhone, PHONE_PATTERN, PHONE_WARN);
+  checkInputValidity(warnMap.phone.input, warnMap.phone.pattern, warnMap.phone.text);
 };
 
 const onEmailInput = function () {
-  checkInputValidity(inputEmail, EMAIL_PATTERN, EMAIL_WARN);
+  checkInputValidity(warnMap.email.input, warnMap.email.pattern, warnMap.email.text);
 };
 
 const addValidationListeners = function () {
