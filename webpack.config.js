106febbb49b120d4b3e7ce3dd939e922ee1e79dd
zubkovvladimir <njoy70@mail.ru>
2021-03-11 11:41:13 +0500

    feat: weback4, terser-webpack and setting webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require(`path`);
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  entry: `./source/js/main.js`,
+  context: path.resolve(__dirname, 'source'),
+  mode: 'none',
+  entry: `./js/main.js`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `build/js`)
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   devtool: `source-map`,
 };
